68f69b59b7dbc427e1286c9e133dc02b2f7a1979
Oleksandr Zhocanuk <mindesync@gmail.com>
2023-07-22 21:42:24 +0300

    Products page done - Product card and list implemented - New util to format a price - Config changes to work with cloudinary URLs

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/types";
 import NoResults from "./UI/no-results";
+import ProductCard from "./UI/product-card";
 
 interface ProductListProps{
     title: string,
@@ -11,6 +12,11 @@ const ProductList: React.FC<ProductListProps> = ({items, title}) => {
     <div className="h-3">
         <h3 className="text-bold text-3xl">{title}</h3>
         {items.length === 0 && <NoResults/>}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+            {items.map((item)=>(
+                <ProductCard key={item.id} data={item}/>
+            ))}
+        </div>
     </div>  
     );
 }
