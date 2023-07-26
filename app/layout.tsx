8ab29533e7b9568f9241a1caf23e9a35186d2f21
Olexander Zhovanyk <mindesync@gmail.com>
2023-07-26 23:59:14 +0300

    Gallery added - New prodcuts page that provide more information about product - Actions to get products

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        {/*//@ts-ignore*/}
         <Navbar/>
         {children}
         <Footer/>
--- components/UI/product-card.tsx
+++ components/UI/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps{
     data: Product,
@@ -12,10 +13,14 @@ interface ProductCardProps{
 
 const ProductCard:React.FC<ProductCardProps> = ({data}) => {
 
-
+    const router = useRouter()
+  
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`)
+    }
 
     return ( 
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div   onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             <div className="aspect-square rounded-xl bg-gray-100 relative ">
                 <Image alt="image" src={data.images?.[0]?.url} fill className="aspect-square rounded-md object-cover"/>
                 <div className="opacity-0 groupe-hover:opacity-100 transition absolute w-full px-6 bottom-5">
